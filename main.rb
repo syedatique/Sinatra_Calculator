73a67b075c7786b982168f710a06c1a48fa4813b
syed <syed.atique@gmail.com>
2015-10-21 18:33:55 +0100

    few update

--- main.rb
+++ main.rb
@@ -3,6 +3,13 @@ require 'sinatra'
 require 'sinatra/contrib/all' if development?
 require 'date'
 
+get '/' do
+
+  erb :home
+
+end
+
+
 get '/age_calculator' do
 
   @dob = params[:dob].to_s
@@ -10,31 +17,25 @@ get '/age_calculator' do
 
  # binding.pry
  today = Date.today
-#   # .strftime("%d/%m/%Y")
-
-case 
-when @dob.year > today.year
+# .strftime("%d/%m/%Y")
+  case 
+  when @dob.year > today.year
    @result = "You are from FUTURE!!"
- when @dob.year == today.year
+  when @dob.year == today.year
    @result = "Your age is: #{0}"
- when @dob.year < today.year
-  if @dob.month > today.month || (@dob.month == today.month && @dob.day > today.day)
-    age = today.year - @dob.year - 1
-  else 
-    age = today.year - @dob.year
-  end
+  when @dob.year < today.year
+    if @dob.month > today.month || (@dob.month == today.month && @dob.day > today.day)
+      age = today.year - @dob.year - 1
+    else 
+      age = today.year - @dob.year
+    end
   @result = "Your age is : #{age}"
-end
+  end
 
 erb :age_calculator
 end
 
-get '/calculat8r' do
-    # ask the user which operation they want to perform
-    # puts `clear`
-    # puts "Good choice - which simple operation do you want to perform?"
-    # print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
-    
+get '/calculat8r' do    
     @first_number = params[:first_number].to_f
     @second_number = params[:second_number].to_f
     @operator = params[:operator]
@@ -56,4 +57,69 @@ get '/calculat8r' do
     end
 
   erb :calculat8r
+end
+
+get '/sleeps' do
+  @dates = params[:dates].to_s
+  @dates = Date.parse(@dates)
+
+  christmas = Date.new(@dates.year, 12, 25)
+  @sleeps = (christmas - @dates).to_i
+
+  @result = case @sleeps
+    when 0
+      "OMG! Check your stockings! Santa's been!!"
+    when 1..10
+      "Only #{sleeps} sleep#{'s' unless sleeps == 1} to go. It's getting close!"
+    when 11..24
+      "The advent calendar is getting a workout with only #{sleeps} sleeps to go."
+    when 25..54
+      "#{sleeps} sleeps until Christmas. Better get the cards for Australian friends written."
+    when 55..100
+      "#{sleeps} sleeps. Check back soon."
+    when 101..250
+      "You're too keen. I feel almost mean telling you there's #{sleeps} sleeps still left."
+    when 251..366
+      "It's hardly even Easter! There still #{sleeps} sleeps until Christmas."
+    else
+      "Oh noes! You missed it! Gotta wait until next year now :-("
+    end
+
+erb :sleeps
+end
+
+get '/measurement_calc' do
+  @measure = params[:measure]
+
+    @result = @measure * case @operator
+    when 'm'
+      1.6093
+    when 'f'
+      0.3048
+    when 'i'
+      2.54
+    when 'k'
+      0.6214
+    when 'e'
+      3.2808
+    when 'c'
+      0.3937
+    end
+
+erb :measurement_calc
+end
+
+
+get '/volume_calc' do
+
+  @vol_cal = params[:vol_cal]
+
+  @result = case @operator
+  when 'c'
+    @vol_cal ** 3
+  when 's'
+    (4.0 / 3.0) * Math::PI * (vol_cal ** 3)
+  end
+
+  erb :volume_calc
 end
